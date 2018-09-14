30088fc68e6bbcd17cd53e6e94a39da42dec534b
Romina Ferrario <saemyferrario@outlook.com>
2018-09-14 08:25:41 +0200

    Snake included

---
+++ src/main/java/ch/bbw/objects/Food.java
@@ -0,0 +1,27 @@
+package ch.bbw.objects;
+
+import javafx.scene.paint.Color;
+
+/**
+ * A simple class to represent food that takes up only one square.
+ *
+ * @author Subhomoy Haldar
+ * @version 2016.12.17
+ */
+public class Food {
+    public static final Color COLOR = Color.ROSYBROWN;
+
+    private Point point;
+
+    public Food(Point point) {
+        this.point = point;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+}
